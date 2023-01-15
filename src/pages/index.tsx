88437e06ee63a79c6eebc31f1710a970dafc1d4d
Adam Andrews <andrewsadam07@gmail.com>
2023-01-15 19:26:02 +0000

    Added Chess functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import Chessboard from '../components/Chessboard';
+import ChessGame from '../components/ChessGame';
 
 export default function Home() {
 	return (
 		<div className="">
-			<Chessboard />
+			<ChessGame />
 		</div>
 	);
 }
